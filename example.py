42673472dcffb7f8688615b9128adf87ac69d4e7
pcomputo <ahuja.pooja22@gmail.com>
2014-09-13 14:41:49 +0530

    Added an example for using the API

---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import googleplay
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if not args:
+        print >> sys.stderr, 'SYNTAX: example.py [app-package-name]'
+        sys.exit(-1)
+        
+    #To accept the package name of the app and pass it to all other functions
+    googleplay.searchApp(args[0])
+    
+    #get the Title of the app
+    googleplay.getAppTitle(1)
+    
+    #get the lastest update date
+    googleplay.getAppUpdateDate(1)
